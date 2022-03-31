97e3a2d3a7647a74face2c8f42f05a870704710b
Mateus Azevedo <mateusazevedoaguiar@gmail.com>
2022-03-31 11:57:20 -0300

    first commit

---
+++ src/components/AreaConteudo.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <h1>Crie seu QRCode</h1>
+    <textarea maxlength="800" rows="6" v-model="inputUsuario"></textarea><br/>
+    <button @click="gerarQrcode()">Gerar QRCode</button><br/>
+    <img id="QrcodeIMG" :src="conteudoQrcode">  
+</div>
+    
+</template>
+<script>
+export default {
+    nome:'AreaConteudo',
+    data() {
+        return{
+            inputUsuario:"",
+            conteudoQrcode:""
+        }
+    },
+    methods: {
+        gerarQrcode(){
+             var inputUsuario2 = this.inputUsuario
+             var googleApi = "https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl="
+             var cont = googleApi + encodeURIComponent(inputUsuario2)
+             this.conteudoQrcode = cont
+        }
+    }
+
+}
+</script>
+<style scoped>
+   h1{
+       margin-bottom: 15px;
+   }
+   div{
+       text-align: center;
+    
+       margin-top: 7vh;
+       
+   }
+   textarea{
+       width: 250px;
+       background-color: #232323;
+       color: white;
+   }
+   img{
+       margin-top: 30px;
+   }
+   button{
+       padding: 20px 63px;
+       font-size: 18px;
+       margin-top: 30px;
+       border: none;
+       border-radius: 7px;
+       cursor: pointer;
+   }
+   
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li><h1>Gerador de QRCode </h1></li>
+            <li class="logo"><img alt="Vue logo" src="../assets/logo.png"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HeaderApp",
+}
+</script>
+<style scoped>
+h1{
+    padding-top: 13px;
+    font-size: 22px;
+    font-family: 'Red Hat Mono', monospace;
+    letter-spacing: 2px;
+}
+.nav{
+    text-align: center;
+    background-color: rgb(27, 27, 27);
+    width: 100%;
+    height: 52px;
+    display: flex;
+    position: relative;
+}
+ul{
+    
+    margin-left: auto;
+    margin-right: auto;
+}
+li{
+   float: left;
+   list-style: none;
+   
+}
+img{
+    width: 30px;
+    height: 30px;
+}
+.logo{
+    margin-top: 13px;
+    margin-left: 7px;
+}
+</style>
